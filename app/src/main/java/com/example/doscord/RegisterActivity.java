806b9dc66753f186b4903b77ddfcd9257216e179
DaOnlyDelta <delta.leskovar@gmail.com>
2026-01-21 13:17:38 +0100

    fixed backtohome

--- app/src/main/java/com/example/doscord/RegisterActivity.java
+++ app/src/main/java/com/example/doscord/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.doscord;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void backToHome(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
